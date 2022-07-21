12948956c5670c4dacdcdd6f2f9c056b0e72359f
Xhivat Hoxhiq <xhivo97@gmail.com>
2022-07-21 07:17:26 +0000

    image: Added image struct and file extension enum.
    Added missing STRLEN macro.
    Now check_extension() returns an extension enum.

--- src/image.c
+++ src/image.c
@@ -3,6 +3,12 @@
 #include <png.h>
 #include "image.h"
 
+enum extension { IMAGE_NOT_SUPPORTED, IMAGE_PNG, IMAGE_JPEG };
+
+struct image {
+    enum extension ext;
+};
+
 #ifdef _WIN32
 static const wchar_t *valid_extensions[] = {L".png", L".PNG", L".jpg", L".JPG", L".jpeg", L".JPEG"};
 #else
@@ -13,6 +19,7 @@ static const char *valid_extensions[] = {".png", ".PNG", ".jpg", ".JPG", ".jpeg"
 int generate_tiles(const wchar_t *in_path, const wchar_t *out_dir) {
 #else
 int generate_tiles(const char *in_path, const char *out_dir) {
+    struct image img;
 #endif
     FILE *fp = FOPEN(in_path, "rb");
     if (!fp) {
@@ -20,7 +27,8 @@ int generate_tiles(const char *in_path, const char *out_dir) {
         return EXIT_FAILURE;
     }
 
-    if (check_extension(in_path) == EXIT_FAILURE) {
+    img.ext = check_extension(in_path);
+    if (img.ext == IMAGE_NOT_SUPPORTED) {
         print_error("file %s is not supported\n", getbase(in_path));
         fclose(fp);
         return EXIT_FAILURE;
@@ -35,25 +43,35 @@ int generate_tiles(const char *in_path, const char *out_dir) {
     return EXIT_SUCCESS;
 }
 
+#ifdef _WIN32
+int check_extension(const wchar_t *in_path) {
+    if (STRLEN(in_path) < 5)
+        return IMAGE_NOT_SUPPORTED;
+    const wchar_t *ext = in_path + STRLEN(in_path)-5;
+#else
 int check_extension(const char *in_path) {
     if (STRLEN(in_path) < 5)
-        return EXIT_FAILURE;
-
+        return IMAGE_NOT_SUPPORTED;
     const char *ext = in_path + STRLEN(in_path)-5;
+#endif
     int size = sizeof(valid_extensions) / sizeof(valid_extensions[0]);
 
     if (ext[0] == '.') {
         for (int i = 0; i < size; i++) {
-            if (strcmp(ext, valid_extensions[i]) == 0)
-                return EXIT_SUCCESS;
+            if (STRCMP(ext, valid_extensions[i]) == 0)
+                return IMAGE_JPEG;
         }
     } else {
         ext++;
         for (int i = 0; i < size; i++) {
-            if (strcmp(ext, valid_extensions[i]) == 0)
-                return EXIT_SUCCESS;
+            if (STRCMP(ext, valid_extensions[i]) == 0) {
+                if (ext[1] == 'p' || ext[1] == 'P')
+                    return IMAGE_PNG;
+
+                return IMAGE_JPEG;
+            }
         }
     }
     
-    return EXIT_FAILURE;
+    return IMAGE_NOT_SUPPORTED;
 }
--- src/platform.h
+++ src/platform.h
@@ -19,11 +19,13 @@
 #define FPRINTF(stream, format, ...) fwprintf(stream, STRING(format), __VA_ARGS__)
 #define FOPEN(filename, mode) _wfopen(filename, STRING(mode))
 #define STRLEN(s) (wcslen(s)+1)
+#define STRCMP(s1, s2) wcscmp(s1, s2)
 #else
 #define STRING(s) s
 #define FPRINTF(stream, format, ...) fprintf(stream, format, __VA_ARGS__)
 #define FOPEN(filename, mode) fopen(filename, mode)
 #define STRLEN(s) strlen(s)
+#define STRCMP(s1, s2) strcmp(s1, s2)
 #endif
 
 #ifdef _WIN32
